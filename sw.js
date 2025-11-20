723a67e829f3c998cc2a04a8f4d2ef255fb3e7f0
SELIGASOLUCOES <marcosmarcaocruz@gmail.com>
2025-11-20 15:13:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = 'solarflow-titanium-v6';
-
+const CACHE_NAME = 'solarflow-platinum-v25';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,7 +6,7 @@ const urlsToCache = [
   './icon-192.png',
   './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Manrope:wght@300;500;700;800&display=swap',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;500;700;800&display=swap',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js'
@@ -25,7 +24,6 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // APIs Externas (NASA/Nominatim) não devem ser cacheadas permanentemente
   if (event.request.url.includes('nasa.gov') || event.request.url.includes('openstreetmap.org')) {
     event.respondWith(fetch(event.request));
     return;
